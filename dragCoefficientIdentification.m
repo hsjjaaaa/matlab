08519a456fb185eb8bc5526213232dc896241ad2
hsjjaaaa <a098100@163.com>
2018-11-15 21:04:11 +0800

    a

--- dragCoefficientIdentification.m
+++ dragCoefficientIdentification.m
@@ -1,10 +1,10 @@
 %PO观测节点压力 GO观测管段流量 S阻力系数
 %数值 节点或管段号 工况
 
-function [] = dragCoefficientIdentification(A,D,Re,u,p,S,e,PO,GO)
+function [sum] = dragCoefficientIdentification(A,D,Re,u,p,S,e,PO,GO)
 sum=0;
 [G_Cal,P_Cal] = hydraulicCalculation(A,D,Re,u,p,S,e);
-%获取工况数
+%获取数据数量
 [p_length,~]=size(PO);
 [g_length,~]=size(GO);
 
